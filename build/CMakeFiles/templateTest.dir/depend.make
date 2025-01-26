6c547f0dd971a9fab1e2a031d29598e9b477184d
rzk <ruanzk2098@gmail.com>
2025-01-26 12:31:20 +0800

    0126

---
+++ build/CMakeFiles/templateTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for templateTest.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/templateTest.dir/src/template/add.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/templateTest.dir/src/template/add.cpp.o: \
+ /home/rzk/document/cpluspluslearning/src/template/add.cpp \
+ /usr/include/stdc-predef.h
